d1172bd451de455fc822fcb998f3214d5d6e0e06
Lucas <pagano.lucas@hotmail.com>
2018-07-22 16:45:04 -0700

    remove checkpoint_every argument and change models paths

--- scripts/automated_run_traj_cnn_multi.sh
+++ scripts/automated_run_traj_cnn_multi.sh
@@ -1,9 +1,8 @@
+#!/usr/bin/env bash
 # use it from the scripts folder
 # usage run_traj_cnn.sh dataset_name restore
 export PYTHONPATH=`cd .. && pwd`
 
-for i in {1..3}
-do
 python3 train_generator_only.py \
   --dataset_name $1 \
   --delim tab \
@@ -15,16 +14,15 @@ python3 train_generator_only.py \
   --dropout 0 \
   --batch_size 32 \
   --g_learning_rate 1e-3 \
-  --checkpoint_every 100 \
   --print_every 200 \
   --num_epochs 50 \
-  --checkpoint_name organized_cnn_$1_5lay_12 \
-  --output_dir organized_cnn_models_multi \
-  --loader_num_workers 8 \
+  --checkpoint_name $1_50epoch \
+  --output_dir save \
+  --loader_num_workers 0 \
   --restore_from_checkpoint $2
 
-echo "TESTING NOW epochs 50" 
-python3 evaluate_generator_only.py --model_path organized_cnn_models_multi/organized_cnn_$1_5lay_12_with_model.pt
+echo "TESTING NOW epochs 50"
+python3 evaluate_generator_only.py --model_path save/$1_50epoch_with_model.pt
 
 python3 train_generator_only.py \
   --dataset_name $1 \
@@ -37,17 +35,15 @@ python3 train_generator_only.py \
   --dropout 0 \
   --batch_size 32 \
   --g_learning_rate 1e-3 \
-  --checkpoint_every 100 \
   --print_every 200 \
   --num_epochs 100 \
-  --checkpoint_name organized_cnn_$1_5lay_12 \
-  --output_dir organized_cnn_models_multi \
-  --loader_num_workers 8 \
-  --restore_from_checkpoint 1
+  --checkpoint_name $1_100epoch \
+  --output_dir save \
+  --loader_num_workers 0 \
+  --restore_from_checkpoint $2
 
 echo "TESTING NOW epochs 100"
-python3 evaluate_generator_only.py --model_path organized_cnn_models_multi/organized_cnn_$1_5lay_12_with_model.pt
-
+python3 evaluate_generator_only.py --model_path save/$1_100epoch_with_model.pt
 
 python3 train_generator_only.py \
   --dataset_name $1 \
@@ -59,16 +55,13 @@ python3 train_generator_only.py \
   --l2_loss_weight 1 \
   --dropout 0 \
   --batch_size 32 \
-  --g_learning_rate 1e-3 \
-  --checkpoint_every 100 \
+  --g_learning_rate 1e-5 \
   --print_every 200 \
-  --num_epochs 150 \
-  --checkpoint_name organized_cnn_$1_5lay_12 \
-  --output_dir organized_cnn_models_multi \
-  --loader_num_workers 8 \
-  --restore_from_checkpoint 1
+  --num_epochs 250 \
+  --checkpoint_name $1_150epoch \
+  --output_dir save \
+  --loader_num_workers 0 \
+  --restore_from_checkpoint $2
 
 echo "TESTING NOW epochs 150"
-python3 evaluate_generator_only.py --model_path organized_cnn_models_multi/organized_cnn_$1_5lay_12_with_model.pt
-
-done
+python3 evaluate_generator_only.py --model_path save/$1_150epoch_with_model.pt
