c56fed01b9fc2205bf714a6ffda977907f7cc5dc
Lucas <pagano.lucas@hotmail.com>
2018-08-08 12:12:06 -0700

    change hidden2pos to be 2 linear layers

--- cnn/model_cnn.py
+++ cnn/model_cnn.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import math
+import numpy as np
 import collections
 
 def Conv1d(in_channels, out_channels, kernel_size, padding, dropout=0):
@@ -9,6 +10,20 @@ def Conv1d(in_channels, out_channels, kernel_size, padding, dropout=0):
     m.weight.data.normal_(mean=0, std=std)
     m.bias.data.zero_()
     return nn.utils.weight_norm(m)
+
+def distance(positions):
+    '''
+    compute the distance between the first and last position in the positions array
+
+    :param positions: array of shape obs_len * 2
+    :return: distance between first and last position
+    '''
+    x1 = positions[0][0]
+    x2 = positions[-1][0]
+    y1 = positions[0][1]
+    y2 = positions[-1][1]
+    return ((positions[0][0] - positions[-1][0] )**2 + (positions[0][1] - positions[-1][1] )**2)**0.5
+
 
 class TrajEstimator(nn.Module):
     def __init__(self, obs_len, pred_len, embedding_dim=16, encoder_h_dim=16, num_layers=3, dropout=0.0):
@@ -30,7 +45,11 @@ class TrajEstimator(nn.Module):
                 conv_dict[str(i)] = Conv1d(in_channels=embedding_dim, out_channels=embedding_dim, kernel_size=3, padding=1, dropout=0)
                 conv_dict[str(i+1)] = nn.ReLU()
         self.convs = nn.Sequential(conv_dict)
-        self.hidden2pos = nn.Linear(embedding_dim * self.obs_len, 2 * self.pred_len)
+        conv_out_size = embedding_dim * self.obs_len
+        self.hidden2pos = nn.Sequential(
+            nn.Linear(conv_out_size + 1, int((conv_out_size+1)/2)),
+            nn.Linear(int((conv_out_size+1)/2), 2 * self.pred_len))
+
 
     def forward(self, obs_traj, obs_traj_rel, seq_start_end, epoch=0):
         """
@@ -42,11 +61,18 @@ class TrajEstimator(nn.Module):
         - pred_traj_rel: Tensor of shape (self.pred_len, batch, 2)
         """
         batch_size = obs_traj.size(1)
+        #compute ade and fde for later
+        view_obs = np.swapaxes(obs_traj.detach().cpu().numpy(), 0, 1)
+        final_dist = torch.from_numpy(np.fromiter((distance(x) for x in view_obs), view_obs.dtype)).cuda().view(batch_size, -1)
+
         obs_traj_embedding = self.spatial_embedding(obs_traj_rel.view(-1, 2))
+        debug_obs = obs_traj.detach().cpu().numpy()
         obs_traj_embedding = obs_traj_embedding.view(-1, batch_size, self.embedding_dim
         ).permute(1, 2, 0)
-        state = self.convs(obs_traj_embedding).view(batch_size, -1)
-        rel_pos = self.hidden2pos(state)
+        state = self.convs(obs_traj_embedding).view(batch_size, self.obs_len * self.embedding_dim)
+        #add ade and fde to state
+        state_fde = torch.cat((state, final_dist), dim=1)
+        rel_pos = self.hidden2pos(state_fde)
         pred_traj_fake_rel = rel_pos.reshape(batch_size, self.pred_len, 2).permute(1, 0, 2)
         return pred_traj_fake_rel
 
