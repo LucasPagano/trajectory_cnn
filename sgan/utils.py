27e33053d23e24efee092377f2027d50c0063fe5
Lucas <pagano.lucas@hotmail.com>
2018-08-14 11:55:55 -0700

    make get_resized_obstacle_maps directory independant

--- sgan/utils.py
+++ sgan/utils.py
@@ -79,7 +79,8 @@ def get_resized_obstacle_maps():
     datasets = ["eth", "hotel", "univ", "zara1", "zara2"]
     obstacle_maps = {}
     for dataset in datasets:
-        obstacle_map_path = os.path.join("../datasets", dataset, "obs_map.pkl")
+        dset_path = os.path.dirname(get_dset_path(dataset, "test"))
+        obstacle_map_path = os.path.join(dset_path, "obs_map.pkl")
         with open(obstacle_map_path, "rb") as obs_map:
             obstacle_maps[dataset] = pickle.load(obs_map)
 
