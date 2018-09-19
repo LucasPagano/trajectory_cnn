a4235272876aef26479288c9735a4a6e63ba1133
Lucas <pagano.lucas@hotmail.com>
2018-09-19 15:48:29 -0700

    add evaluation

--- scripts/automated_run_cnn_all_datasets.sh
+++ scripts/automated_run_cnn_all_datasets.sh
@@ -3,6 +3,9 @@
 
 for item in eth hotel univ zara1 zara2;
 do
-  echo RUNNING automated_run_traj_cnn_multi.sh on $item
- /bin/bash automated_run_traj_cnn_multi.sh $item 0
+  echo RUNNING automated_run_traj_cnn.sh on $item
+ /bin/bash automated_run_traj_cnn.sh $item 0
 done
+
+echo EVALUATING on all datasets
+python3 evaluate_cnn_all_datasets.py
